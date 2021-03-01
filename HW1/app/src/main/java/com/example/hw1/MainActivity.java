eafdfcd1e8d013969ae00d5aa1bfefac428adfa4
hesanghasemi <hesanghasemi99@gmail.com>
2021-03-01 21:16:31 +0330

    messing around

--- HW1/app/src/main/java/com/example/hw1/MainActivity.java
+++ HW1/app/src/main/java/com/example/hw1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hw1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ HW1/app/src/main/java/com/example/hw1/activities/CandlesActivity.java
@@ -0,0 +1,16 @@
+package com.example.hw1.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hw1.R;
+
+public class CandlesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+    }
+}
