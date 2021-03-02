ab3d7088c3eb7b66dbbd55c95d58a1872a5384f2
hesanghasemi <hesanghasemi99@gmail.com>
2021-03-02 12:28:18 +0330

    added charts to project.

--- HW1/app/src/main/java/com/example/hw1/MainActivity.java
+++ HW1/app/src/main/java/com/example/hw1/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.hw1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_candles);
+
+        findViewById(R.id.buttonBarChart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BarChartActivity.class));
+            }
+        });
+
+        findViewById(R.id.buttonPieChart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PieChartActivity.class));
+            }
+        });
+
+        findViewById(R.id.buttonRadarChart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RadarChartActivity.class));
+            }
+        });
     }
 }
