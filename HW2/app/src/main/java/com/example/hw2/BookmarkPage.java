96f7ce6aa921e92f09533adc1306d00372edf4a9
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-04-30 18:17:53 +0430

    add bookmark page template

---
+++ HW2/app/src/main/java/com/example/hw2/BookmarkPage.java
@@ -0,0 +1,25 @@
+package com.example.hw2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BookmarkPage extends Fragment
+{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_bookmark_page, container, false);
+    }
+}
