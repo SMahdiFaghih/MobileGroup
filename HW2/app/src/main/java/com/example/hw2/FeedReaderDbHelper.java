c811d2ecf1dbced9b5cc029269856e8767f0b3a0
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-04-30 22:08:26 +0430

    added BookmarkManager for SQLite instructions

---
+++ HW2/app/src/main/java/com/example/hw2/FeedReaderDbHelper.java
@@ -0,0 +1,29 @@
+package com.example.hw2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FeedReaderDbHelper extends SQLiteOpenHelper
+{
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Bookmark.db";
+
+    public FeedReaderDbHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        db.execSQL(FeedReaderContract.SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL(FeedReaderContract.SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+}
