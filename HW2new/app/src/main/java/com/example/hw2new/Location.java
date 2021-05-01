6d70c8740e264b040db12530558f51b770999309
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-05-01 20:09:14 +0430

    added previous files to this new project

---
+++ HW2new/app/src/main/java/com/example/hw2new/Location.java
@@ -0,0 +1,30 @@
+package com.example.hw2new;
+
+public class Location
+{
+    private String locationName;
+    private String x;
+    private String y;
+
+    public Location(String locationName, String x, String y)
+    {
+        this.locationName = locationName;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLocationName()
+    {
+        return locationName;
+    }
+
+    public String getX()
+    {
+        return x;
+    }
+
+    public String getY()
+    {
+        return y;
+    }
+}
