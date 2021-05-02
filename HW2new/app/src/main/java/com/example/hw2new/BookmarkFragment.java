b68adfc5e834d2e87f43df0fb883e5ce898ce974
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-05-02 21:02:40 +0430

    set bookmarkFragment data dynamically

--- HW2new/app/src/main/java/com/example/hw2new/BookmarkFragment.java
+++ HW2new/app/src/main/java/com/example/hw2new/BookmarkFragment.java
@@ -7,9 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BookmarkFragment extends Fragment
 {
+    private String locationName;
+    private String x;
+    private String y;
+
+    public static BookmarkFragment newInstance(String locationName, String x, String y)
+    {
+        BookmarkFragment fragment = new BookmarkFragment();
+        fragment.locationName = locationName;
+        fragment.x = x;
+        fragment.y = y;
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -19,6 +35,37 @@ public class BookmarkFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_bookmark, container, false);
+        View view = inflater.inflate(R.layout.fragment_bookmark, null);
+
+        Button button = (Button) view.findViewById(R.id.bookmark_button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Show this on map
+            }
+        });
+
+        ImageView trashBinIcon = (ImageView) view.findViewById(R.id.trashBinIcon);
+        trashBinIcon.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                BookmarkManager.getInstance().deleteLocation(locationName);
+            }
+        });
+
+        TextView locationNameTextView = (TextView) view.findViewById(R.id.location_name);
+        locationNameTextView.setText(locationName);
+
+        TextView xTextView = (TextView) view.findViewById(R.id.location_x);
+        xTextView.setText(x);
+
+        TextView yTextView = (TextView) view.findViewById(R.id.location_y);
+        yTextView.setText(y);
+
+        return view;
     }
 }
