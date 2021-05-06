96da848b3506d4c855edc97a6cee8d9ffd6096f0
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-05-06 16:05:48 +0430

    added bookmark from HW2new to Test

---
+++ Test/app/src/main/java/com/example/mytest/BookmarkManager.java
@@ -0,0 +1,84 @@
+package com.example.mytest;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+import java.util.ArrayList;
+
+public class BookmarkManager
+{
+    private static BookmarkManager Instance;
+    private FeedReaderDbHelper dbHelper;
+
+    private BookmarkManager()
+    {
+        //Only to make this class Singleton
+    }
+
+    public void initialize(Context context)
+    {
+        dbHelper = new FeedReaderDbHelper(context);
+    }
+
+    public void insertNewLocation(String locationName, String x, String y)
+    {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME, locationName);
+        values.put(FeedReaderContract.FeedEntry.COLUMN_X, x);
+        values.put(FeedReaderContract.FeedEntry.COLUMN_Y, y);
+
+        db.insert(FeedReaderContract.FeedEntry.TABLE, null, values);
+    }
+
+    public ArrayList<Location> getAllLocations()
+    {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        ArrayList<Location> locations = new ArrayList<>();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM " + FeedReaderContract.FeedEntry.TABLE, null);
+        if (cursor.moveToFirst())
+        {
+            while (!cursor.isAfterLast())
+            {
+                String locationName = cursor.getString(cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_NAME));
+                String x = cursor.getString(cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_X));
+                String y = cursor.getString(cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_Y));
+                Location location = new Location(locationName, x, y);
+                locations.add(location);
+
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+        return locations;
+    }
+
+    public void deleteLocation(String locationName)
+    {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME + " = ?";
+        String[] selectionArgs = { locationName };
+        db.delete(FeedReaderContract.FeedEntry.TABLE, selection, selectionArgs);
+    }
+
+    public void deleteAllLocations()
+    {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.delete(FeedReaderContract.FeedEntry.TABLE,null,null);
+    }
+
+    public static BookmarkManager getInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = new BookmarkManager();
+        }
+        return Instance;
+    }
+}
---
+++ Test/app/src/main/java/com/example/mytest/FeedReaderContract.java
@@ -0,0 +1,27 @@
+package com.example.mytest;
+
+import android.provider.BaseColumns;
+
+public final class FeedReaderContract
+{
+    private FeedReaderContract()
+    {
+        //No instance of this class
+    }
+
+    public static class FeedEntry implements BaseColumns
+    {
+        public static final String TABLE = "bookmark";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_X = "x";
+        public static final String COLUMN_Y = "y";
+    }
+
+    public static final String SQL_CREATE_ENTRIES = "CREATE TABLE IF NOT EXISTS " + FeedEntry.TABLE + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    FeedEntry.COLUMN_NAME + " TEXT NOT NULL UNIQUE," +
+                    FeedEntry.COLUMN_X + " TEXT NOT NULL," +
+                    FeedEntry.COLUMN_Y + " TEXT NOT NULL)";
+
+    public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + FeedEntry.TABLE;
+}
