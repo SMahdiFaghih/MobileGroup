993c6acd6225ab276e320e1a00edef29381b65f5
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-05-08 01:57:24 +0430

    fixed bookmark search bug

--- Test/app/src/main/java/com/example/mytest/BookmarkPageFragment.java
+++ Test/app/src/main/java/com/example/mytest/BookmarkPageFragment.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 
-import java.io.Console;
 import java.util.ArrayList;
 
 public class BookmarkPageFragment extends Fragment
@@ -69,11 +68,19 @@ public class BookmarkPageFragment extends Fragment
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         for (Location location : locations)
         {
-            if (!fragmentManager.getFragments().contains(fragmentManager.findFragmentByTag("fragment " + location.getLocationName())))
-            {
-                BookmarkFragment bookmarkFragment = BookmarkFragment.newInstance(location);
-                fragmentTransaction.add(R.id.bookmarksList, bookmarkFragment, "fragment " + location.getLocationName());
-            }
+            BookmarkFragment bookmarkFragment = BookmarkFragment.newInstance(location);
+            fragmentTransaction.add(R.id.bookmarksList, bookmarkFragment, "fragment " + location.getLocationName());
+        }
+        fragmentTransaction.commit();
+    }
+
+    private void showHiddenBookmarks()
+    {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        for (Location location : locations)
+        {
+            fragmentTransaction.show(fragmentManager.findFragmentByTag("fragment " + location.getLocationName()));
         }
         fragmentTransaction.commit();
     }
@@ -82,7 +89,7 @@ public class BookmarkPageFragment extends Fragment
     {
         if (searchedString == null || searchedString.equals(""))
         {
-            showAllBookmarks();
+            showHiddenBookmarks();
             return;
         }
 
@@ -95,7 +102,7 @@ public class BookmarkPageFragment extends Fragment
                 Fragment fragment = fragmentManager.findFragmentByTag("fragment " + location.getLocationName());
                 if (fragment != null)
                 {
-                    fragmentTransaction.remove(fragment);
+                    fragmentTransaction.hide(fragment);
                 }
             }
         }
