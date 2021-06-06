5c8b0190632ffa7aaaa602d14d990e4e38d1ddad
hesanghasemi <hesanghasemi99@gmail.com>
2021-06-06 19:05:13 +0430

    changed swift file

--- Swift/swift.swift
+++ Swift/swift.swift
@@ -21,62 +21,65 @@ class Todo {
  
     func setPriority(newPriority: Int) { 
         priority = newPriority 
-    } 
+    }
 } 
- 
-var listOfTodoItems = [] 
- 
+
+var listOfTodoItems = [Todo]()
+
 print("Hi! Welcome to our todo app...") 
 print("Please write what you want to do...") 
 print("createTodoItem - updateTodoItem - removeTodoItem - showAllTodoItems") 
-let userInput = readLine() 
+
+let userInput = readLine()
 if (userInput == "createTodoItem") { 
     print("Item name:") 
-    let name = readLine() 
+    let name = readLine()
     print("Item content:") 
     let content = readLine() 
     print("Item priority:") 
-    let priority = Int(readLine()) 
-    let todoItem = Todo(title: name, content: content, priority: priority) 
+    let priority = readLine()
+    let todoItem = Todo(title: name, content: content, priority: Int(priority)) 
     listOfTodoItems.append(todoItem) 
-} else if (userInput == "updateTodoItem") { 
+}
+else if (userInput == "updateTodoItem") { 
     print("Enter the name of item you want to update:") 
-    let name = readLine() 
-    let selectedItem = nil 
+    let name = readLine()
+    var selectedItem = Todo(title: "", content: "", priority: -1)
     for item in listOfTodoItems { 
-        if (item.title == name) { 
+        if (item.title == name) {
             selectedItem = item 
         } 
-    } 
-    if (selectedItem != nil) { 
+    }
+    if (selectedItem.title != "") { 
         print("Enter new name:") 
-        let newName = readLine() 
+        let newName = readLine()
         print("Enter new content:") 
         let newContent = readLine() 
         print("Enter new priority:") 
-        let newPriority = Int(readLine()) 
-        selectedItem.setTitle(newName) 
-        selectedItem.setContent(newContent) 
-        selectedItem.setPriority(newPriority)         
+        let newPriority = readLine()
+        selectedItem.setTitle(newTitle: newName) 
+        selectedItem.setContent(newContent: newContent) 
+        selectedItem.setPriority(newPriority: Int(newPriority))         
     } else { 
         print("Item with this name doesn't exist") 
     } 
-} else if (userInput == "removeTodoItem") { 
+}
+else if (userInput == "removeTodoItem") { 
     print("Enter the name of item you want to remove:") 
     let name = readLine() 
-    let selectedItemIndex = nil 
+    var selectedItemIndex = -1
     for i in 0..<listOfTodoItems.count { 
         if (listOfTodoItems[i].title == name) { 
             selectedItemIndex = i 
         } 
     } 
-    if (selectedItemIndex != nil) { 
+    if (selectedItemIndex != -1) { 
         listOfTodoItems.remove(at: selectedItemIndex) 
     } else { 
         print("Item with this name doesn't exist") 
     } 
 } else { 
     for item in listOfTodoItems { 
-        print(item.title + "-" + item.content + "-" + item.priority) 
+        print(item.title + "-" + item.content + "-" + String(item.priority)) 
     } 
 }
