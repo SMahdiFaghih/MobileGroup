6ae47c5a018754629001dc545eb1b459dd109993
mohammadreza <mohammad0reza79@gmail.com>
2021-06-07 20:00:43 +0430

    sort todoItems done

--- Swift/swift.swift
+++ Swift/swift.swift
@@ -1,228 +1,276 @@
-import Foundation 
- 
-class Todo 
-{ 
-    var title = "" 
-    var content = "" 
-    var priority = 0 
- 
-    init(title: String, content: String, priority: Int) 
-    { 
-        self.title = title 
-        self.content = content 
-        self.priority = priority 
-    } 
- 
-    func setTitle(newTitle: String) 
-    { 
-        title = newTitle 
-    } 
- 
-    func setContent(newContent: String) 
-    { 
-        content = newContent 
-    } 
- 
-    func setPriority(newPriority: Int) 
-    { 
-        priority = newPriority 
-    }
-} 
+import Foundation
 
-class Category 
+class Todo
+{
+    var title = ""
+    var content = ""
+    var priority = 0
+
+    init(title: String, content: String, priority: Int)
+    {
+        self.title = title
+        self.content = content
+        self.priority = priority
+    }
+
+    func setTitle(newTitle: String)
+    {
+        title = newTitle
+    }
+
+    func setContent(newContent: String)
+    {
+        content = newContent
+    }
+
+    func setPriority(newPriority: Int)
+    {
+        priority = newPriority
+    }
+}
+
+class Category
 {
     var name = ""
     var listOfTodoItemsInThisCategory = [Todo]()
 
-    init(name: String) 
-    { 
+    init(name: String)
+    {
         self.name = name
-    } 
+    }
 }
 
 var listOfTodoItems = [Todo]()
 var listOfCategories = [Category]()
 
-print("Hi! Welcome to our todo app...") 
-print("Please write what you want to do...") 
-print("createTodoItem - updateTodoItem - removeTodoItem - showAllTodoItems") 
-print("createCategory - addTodoItemToCategory - showAllCategoryTodoItems") 
+print("Hi! Welcome to our todo app...")
+print("Please write what you want to do...")
+print("createTodoItem - updateTodoItem - removeTodoItem - showAllTodoItems - sortTodoItems")
+print("createCategory - addTodoItemToCategory - showAllCategoryTodoItems")
 
 while (true)
 {
-    if let userInput = readLine() 
+    if let userInput = readLine()
     {
-        if (userInput == "createTodoItem") 
-        { 
-            print("Item name:") 
+        if (userInput == "createTodoItem")
+        {
+            print("Item name:")
             let name = readLine()
-            print("Item content:") 
-            let content = readLine() 
-            print("Item priority:") 
+            print("Item content:")
+            let content = readLine()
+            print("Item priority:")
             let priority = readLine()
-            let todoItem = Todo(title: name!, content: content!, priority: Int(priority!)!) 
-            listOfTodoItems.append(todoItem) 
-            print("New Todo Item added.") 
+            let todoItem = Todo(title: name!, content: content!, priority: Int(priority!)!)
+            listOfTodoItems.append(todoItem)
+            print("New Todo Item added.")
         }
-        else if (userInput == "updateTodoItem") 
-        { 
-            print("Enter the name of item you want to update:") 
+        else if (userInput == "updateTodoItem")
+        {
+            print("Enter the name of item you want to update:")
             let name = readLine()
             var selectedItem = Todo(title: "", content: "", priority: -1)
-            for item in listOfTodoItems 
-            { 
-                if (item.title == name) 
+            for item in listOfTodoItems
+            {
+                if (item.title == name)
                 {
-                    selectedItem = item 
-                } 
+                    selectedItem = item
+                }
             }
-            if (selectedItem.title != "") 
-            { 
-                print("Enter new name:") 
+            if (selectedItem.title != "")
+            {
+                print("Enter new name:")
                 let newName = readLine()
-                print("Enter new content:") 
-                let newContent = readLine() 
-                print("Enter new priority:") 
+                print("Enter new content:")
+                let newContent = readLine()
+                print("Enter new priority:")
                 let newPriority = readLine()
-                selectedItem.setTitle(newTitle: newName!) 
-                selectedItem.setContent(newContent: newContent!) 
-                selectedItem.setPriority(newPriority: Int(newPriority!)!)      
-                print("Todo Item updated.")   
-            } 
-            else 
-            { 
-                print("Item with this name doesn't exist") 
-            } 
+                selectedItem.setTitle(newTitle: newName!)
+                selectedItem.setContent(newContent: newContent!)
+                selectedItem.setPriority(newPriority: Int(newPriority!)!)
+                print("Todo Item updated.")
+            }
+            else
+            {
+                print("Item with this name doesn't exist")
+            }
         }
-        else if (userInput == "removeTodoItem") 
-        { 
-            print("Enter the name of item you want to remove:") 
-            let name = readLine() 
+        else if (userInput == "removeTodoItem")
+        {
+            print("Enter the name of item you want to remove:")
+            let name = readLine()
             var selectedItemIndex = -1
-            for i in 0..<listOfTodoItems.count 
-            { 
-                if (listOfTodoItems[i].title == name) 
-                { 
-                    selectedItemIndex = i 
-                } 
-            } 
-            if (selectedItemIndex != -1) 
-            { 
+            for i in 0..<listOfTodoItems.count
+            {
+                if (listOfTodoItems[i].title == name)
+                {
+                    selectedItemIndex = i
+                }
+            }
+            if (selectedItemIndex != -1)
+            {
                 listOfTodoItems.remove(at: selectedItemIndex)
-                for category in listOfCategories 
-                { 
+                for category in listOfCategories
+                {
                     selectedItemIndex = -1
-                    for i in 0..<category.listOfTodoItemsInThisCategory.count 
-                    { 
-                        if (listOfTodoItems[i].title == name) 
-                        { 
-                            selectedItemIndex = i 
-                        } 
-                    } 
+                    for i in 0..<category.listOfTodoItemsInThisCategory.count
+                    {
+                        if (listOfTodoItems[i].title == name)
+                        {
+                            selectedItemIndex = i
+                        }
+                    }
                     if (selectedItemIndex != -1)
                     {
                         category.listOfTodoItemsInThisCategory.remove(at: selectedItemIndex)
                     }
                 }
-                print("Todo Item removed.") 
+                print("Todo Item removed.")
+            }
+            else
+            {
+                print("Item with this name doesn't exist")
+            }
+        }
+        else if (userInput == "sortTodoItems")
+        {
+            print("which property you want to sort by?")
+            print("title - priority - date")
+            let sortBy = readLine()
+            print("Ascending or Descending?")
+            print("ascending - descending")
+            let sortType = readLine()
+
+            if (sortBy == "title") 
+            {
+                if (sortType == "ascending") 
+                {
+                    listOfTodoItems = listOfTodoItems.sorted(by: { $0.title < $1.title })
+                } 
+                else 
+                {
+                    listOfTodoItems = listOfTodoItems.sorted(by: { $0.title > $1.title })
+                }
             } 
-            else 
-            { 
-                print("Item with this name doesn't exist") 
+            else if (sortBy == "priority") 
+            {
+                if (sortType == "ascending") 
+                {
+                    listOfTodoItems = listOfTodoItems.sorted(by: { $0.priority < $1.priority })
+                } 
+                else 
+                {
+                    listOfTodoItems = listOfTodoItems.sorted(by: { $0.priority > $1.priority })
+                }
             } 
-        } 
-        else if (userInput == "createCategory") 
-        { 
-            print("Category name:") 
+            else {
+                if (sortType == "ascending") 
+                {
+                    listOfTodoItems = listOfTodoItems.sorted(by: { $0.dateCreated < $1.dateCreated })
+                } 
+                else 
+                {
+                    listOfTodoItems = listOfTodoItems.sorted(by: { $0.dateCreated > $1.dateCreated })
+                }
+            }
+
+            for item in listOfTodoItems 
+            {
+                print(item.title + " - " + item.content + " - " + String(item.priority))
+            }
+
+        }
+        else if (userInput == "createCategory")
+        {
+            print("Category name:")
             let name = readLine()
             var isNameRepititive = false
-            for category in listOfCategories 
-            { 
-                if (category.name == name) 
+            for category in listOfCategories
+            {
+                if (category.name == name)
                 {
                     isNameRepititive = true
                 }
-            } 
-            if (!isNameRepititive) 
+            }
+            if (!isNameRepititive)
             {
-                let category = Category(name: name!) 
+                let category = Category(name: name!)
                 listOfCategories.append(category)
                 print("New Category added.")
-            } 
-            else 
+            }
+            else
             {
                 print("A category with this name already exists.")
-            }          
-        } 
-        else if (userInput == "addTodoItemToCategory") 
-        { 
-            print("Item name:") 
+            }
+        }
+        else if (userInput == "addTodoItemToCategory")
+        {
+            print("Item name:")
             let itemName = readLine()
             var selectedItem = Todo(title: "", content: "", priority: -1)
-            for item in listOfTodoItems 
-            { 
+            for item in listOfTodoItems
+            {
                 if (item.title == itemName)
                 {
-                    selectedItem = item 
-                } 
+                    selectedItem = item
+                }
             }
-            if (selectedItem.title != "") 
-            { 
-                print("Category name:") 
+            if (selectedItem.title != "")
+            {
+                print("Category name:")
                 let categoryName = readLine()
                 var selectedCategory = Category(name: "")
-                for category in listOfCategories 
-                { 
+                for category in listOfCategories
+                {
                     if (category.name == categoryName)
                     {
-                        selectedCategory = category 
-                    } 
+                        selectedCategory = category
+                    }
                 }
-                if (selectedCategory.name != "") 
-                { 
+                if (selectedCategory.name != "")
+                {
                     selectedCategory.listOfTodoItemsInThisCategory.append(selectedItem)
                     print("Item added to the Category")
-                } 
-                else 
-                { 
-                    print("Category with this name doesn't exist") 
-                } 
-            } 
-            else 
-            { 
-                print("Item with this name doesn't exist") 
-            }         
-        } 
-        else if (userInput == "showAllCategoryTodoItems") 
-        { 
-            print("Category name:") 
+                }
+                else
+                {
+                    print("Category with this name doesn't exist")
+                }
+            }
+            else
+            {
+                print("Item with this name doesn't exist")
+            }
+        }
+        else if (userInput == "showAllCategoryTodoItems")
+        {
+            print("Category name:")
             let categoryName = readLine()
             var selectedCategory = Category(name: "")
-            for category in listOfCategories 
-            { 
+            for category in listOfCategories
+            {
                 if (category.name == categoryName)
                 {
-                    selectedCategory = category 
-                } 
+                    selectedCategory = category
+                }
             }
-            if (selectedCategory.name != "") 
-            { 
+            if (selectedCategory.name != "")
+            {
                 for item in selectedCategory.listOfTodoItemsInThisCategory
-                { 
-                    print(item.title + "-" + item.content + "-" + String(item.priority)) 
-                } 
-            } 
-            else 
-            { 
-                print("Category with this name doesn't exist") 
-            }         
-        } 
-        else 
-        { 
-            for item in listOfTodoItems 
-            { 
-                print(item.title + "-" + item.content + "-" + String(item.priority)) 
-            } 
+                {
+                    print(item.title + "-" + item.content + "-" + String(item.priority))
+                }
+            }
+            else
+            {
+                print("Category with this name doesn't exist")
+            }
+        }
+        else
+        {
+            for item in listOfTodoItems
+            {
+                print(item.title + " - " + item.content + " - " + String(item.priority))
+            }
         }
     }
 }
