7f8d67822630933ea4adfed51421ba5f9add04d7
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-07-26 22:53:40 +0430

    added background sound

--- Project/app/src/main/java/com/example/project/MainActivity.java
+++ Project/app/src/main/java/com/example/project/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private static MediaPlayer backgroundMusic;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        backgroundMusic = MediaPlayer.create(getApplicationContext(),R.raw.star_sky);
+        backgroundMusic.setLooping(true);
+        backgroundMusic.start();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DatabaseManager.getInstance().initialize(this);
